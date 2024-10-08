07491f7f867c1bbdca80310d6d327f7028694b9f
dpolaczek11 <danielpolaczek11@gmail.com>
2024-10-08 13:43:29 +0200

    basicConfig11

---
+++ helpers/data.js
@@ -0,0 +1,6 @@
+import 'dotenv/config'
+
+export const userID = "38b10f8c-5c16-47a0-9555-a39bec7a9e3d"
+export const baseUrl = "https://demoqa.com"
+export const user = "daniel_p"
+export const secretPassword = process.env.SECRED_PASSWORD
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,10 +1,93 @@
 import pkg from 'pactum';
 const { spec } = pkg;
-import { expect } from "chai";
+import { expect, use } from "chai";
+import { baseUrl, userID, secretPassword, user } from '../helpers/data.js';
+
+let token;
 
 describe("API tests", () => {
-    it("first test", async () => {
-        const response = await spec().get("https://demoqa.com/BookStore/v1/Books");
+    it.skip("first test", async () => {
+        const response = await spec()
+            .get(`${baseUrl}/BookStore/v1/Books`);
         expect(response.statusCode).to.eql(200);
+        expect(response.body.books[0].title).to.eql("Git Pocket Guide");
+        expect(response.body.books[4].author).to.eql("Kyle Simpson");
     })
+
+    it.skip("create account", async () => {
+        const response = await spec()
+            .post(`${baseUrl}/Account/v1/User`)
+            .withBody({
+                userName: user,
+                password: secretPassword,
+            });
+            expect(response.statusCode).to.eql(201);
+            
+    })
+
+    it("generate token", async () => {
+        const response = await spec()
+          .post(`${baseUrl}/Account/v1/GenerateToken`)
+          .withBody({
+            userName: user,
+            password: secretPassword,
+          });
+          expect(response.statusCode).to.eql(200);
+          token = response.body.token;
+         // console.log(token);
+          
+    })
+
+    it("get user id", async () => {
+        const response = await spec()
+        .get(`${baseUrl}/Account/v1/User/${userID}`)
+        .withBearerToken(token)
+        expect(response.statusCode).to.eql(200);
+          
+    })
+
+    it("add book", async () => {
+        const response = await spec()
+        .post(`${baseUrl}/BookStore/v1/Books`)
+        .withBearerToken(token)
+        .withBody({
+            "userId": userID,
+            "collectionOfIsbns": [
+                {
+                 "isbn": "9781449325862",
+                 "isbn": "9781449331818",
+                 "isbn": "9781449337711"
+                }
+            ] 
+        })
+        
+        expect(response.statusCode).to.eql(201);
+          
+    })
+
+    it("delete book", async () => {
+        const response = await spec()
+        .delete(`${baseUrl}/BookStore/v1/Book`)
+        .withBearerToken(token)
+        .withBody({
+            "isbn": "9781449331818",
+            "userId": userID
+        })
+        //.inspect()
+        
+        expect(response.statusCode).to.eql(204);
+          
+    })
+
+    it("delete all books", async () => {
+        const response = await spec()
+        .delete(`${baseUrl}/BookStore/v1/Books?UserId=${userID}`)
+        .withBearerToken(token)
+        
+        //.inspect()
+        
+        expect(response.statusCode).to.eql(204);
+          
+    })
+
 });
